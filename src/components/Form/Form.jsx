4e7da0ee5c94604705c8ac6562ae0d72961da4d1
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-04-06 16:13:55 +0200

    practice2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,46 @@
-// import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
 
-const Form = () => {
-  return <h2>Form</h2>;
+const Form = ({ onAdd }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    /*onAdd({
+      text: event.target.elements.text.value,
+    })
+    event.target.reset();*/
+
+    const trimmedValue = inputValue.trim();
+
+    if (trimmedValue !== '') {
+      onAdd({text:trimmedValue});
+      setInputValue('');
+    }
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        type="text"
+        placeholder="What do you want to write?"
+        name="search"
+        autoFocus
+        value={inputValue}
+        onChange={handleChange}
+      />
+    </form>
+  );
 };
 
 export default Form;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,76 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
 import Text from '../components/Text/Text';
+import Form from '../components/Form/Form'
+import TodoList from '../components/TodoList/TodoList';
+import EditForm from '../components/EditForm/EditForm';
+
 
 const Todos = () => {
+  //const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("change-todos");
+    return savedTodos ? JSON.parse(savedTodos) : []
+  });
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState(null);
+
+  const onEdit = (todo) => {
+    setIsEditing(true);
+    setCurrentTodo(todo);
+  };
+  
+  const cancelUpdate = () => {
+    setIsEditing(false);
+    setCurrentTodo(null);
+  };
+  
+  const updateTodo = (newText) => {
+    setTodos(prev =>
+      prev.map(todo =>
+        todo.id === currentTodo.id ? { ...todo, text: newText } : todo
+      )
+    );
+    setIsEditing(false);
+    setCurrentTodo(null);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("change-todos", JSON.stringify(todos));
+  }, [todos]); 
+  const addTodos = (todoData) => {
+    const newTodo = {
+      id: nanoid(),
+      text: todoData.text,
+    }
+    setTodos(prevtTodos => {
+      return [...prevtTodos, newTodo];
+    })
+  }
+
+  const deleteTodo = (todoId) => {
+    setTodos(prevtTodos => {
+      return prevtTodos.filter((todo) => todo.id !== todoId);
+    })
+  };
+    const addNewTodo = inputValue => {
+      console.log(inputValue); // має вивести значення інпуту під час сабміту форми
+    };
+    
+    //const todos = [
+     // { id: '1', text: 'Practice more' },
+     // { id: '2', text: 'Get all tasks done on time' },];
+    
   return (
     <>
       <Text textAlign="center">There are no any todos ...</Text>
+      {isEditing ? (
+     <EditForm
+      defaultValue={currentTodo.text} updateTodo={updateTodo} cancelUpdate={cancelUpdate}/> ) 
+      : (<Form onSubmit={addNewTodo} onAdd={addTodos} />)}
+      
+      <TodoList todos={todos} onDelete = {deleteTodo} onEdit={onEdit}/>
     </>
   );
 };
